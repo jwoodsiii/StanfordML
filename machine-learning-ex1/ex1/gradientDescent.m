bfd295a4349a0451f35387b2543dd7707a86cd42
jwoodsiii <woods3.jessie@gmail.com>
2020-02-12 23:03:13 -0500

    Completed Required exercises of Linear Regression project

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,10 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+x = X(:,2);
+h = theta(1) + theta(2)*x;
 
+theta_0 = theta(1) - (alpha/m) * sum(h-y);
+theta_1 = theta(2) - (alpha/m) * sum((h-y).* x);
 
-
-
+theta = [theta_0; theta_1];
 
 
 
