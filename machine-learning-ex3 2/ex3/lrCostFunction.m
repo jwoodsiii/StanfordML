5d83f78c48eb8b0fd6310a647fbed6dd6a551035
jwoodsiii <woods3.jessie@gmail.com>
2020-03-01 15:15:51 -0500

    Action Required
    Need to udpate local files to match versioning in matlab online, which is where the project was submitted from.  For some reason 'ex3data.mat'  wasn't loading the correct variables into my local environment, but everything compiled and ran successfully online.

--- machine-learning-ex3 2/ex3/lrCostFunction.m
+++ machine-learning-ex3 2/ex3/lrCostFunction.m
@@ -38,15 +38,14 @@ grad = zeros(size(theta));
 
 
 
+H = sigmoid(X*theta);
+T = y.*log(H) + (1 - y).*log(1 - H);
+J = -1/m*sum(T) + lambda/(2*m)*sum(theta(2:end).^2);
 
-
-
-
-
+ta = [0; theta(2:end)];
+grad = X'*(H - y)/m + lambda/m*ta;
 
 
 % =============================================================
-
-grad = grad(:);
 
 end
--- machine-learning-ex3 2/ex3/predictOneVsAll.m
+++ machine-learning-ex3 2/ex3/predictOneVsAll.m
@@ -30,7 +30,8 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
-
+C = sigmoid(X*all_theta');
+[M , p] = max(C , [] , 2);
 
 
 
