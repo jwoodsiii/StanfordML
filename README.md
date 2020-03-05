e17b3ae0ea20ab0310aef32d7b948233c70e81dc
jwoodsiii <woods3.jessie@gmail.com>
2020-03-05 14:04:43 -0500

    Sigmoid Gradient
    Completed sigmoid gradient function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+---
+noteId: "929059705efa11eab73c8b401370377d"
+tags: []
+
+---
+
 # StanfordML
  Projects from Andrew Ng's Machine Learning Course 
--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -13,8 +13,14 @@ g = zeros(size(z));
 %               each value of z (z can be a matrix, vector or scalar).
 
 
+% original sigmoid function from sigmoid.m g = 1.0 ./ (1.0 + exp(-z));
+% We can either leverage the function we defined there like so:
 
+g = sigmoid(z) .* (1 - sigmoid(z));
 
+% Or we can recycle that code directly in this function for a slightly more verbose implementation.
+% gGrad = (1.0 ./ (1.0 + exp(-z)) .* (1 - (1.0 ./ (1.0 + exp(-z)))))
+% Since we already have sigmoid defined there's no real reason to use the line above.
 
 
 
